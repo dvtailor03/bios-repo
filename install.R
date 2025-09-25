b5e9a1dd605692271e184e5432d98bf98f209af1
Divya Tailor <111465382+dvtailor03@users.noreply.github.com>
2025-09-24 22:43:53 -0400

    add Rtsne

--- install.R
+++ install.R
@@ -2,5 +2,6 @@ install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
 install.packages("skimr", dependencies = TRUE)
+install.packages("Rtsne", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
